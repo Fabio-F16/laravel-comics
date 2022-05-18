9722e86cd1ceea97abe543c071def20b32625905
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-18 17:02:10 +0200

    add main

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -1,9 +1,27 @@
 @extends ('layouts.app')
 
 @section('title')
-    pagina principale
+   laravel comics
 @endsection
 
+
+<!-- start content -->
 @section('content')
-  content
+<main>
+  <div class="container">
+    <div class="row justify-content-center">
+      @foreach ($comics as $comic)
+          <div class="col-12 col-md-2 m-3 text-center">
+            
+            <img src="{{ $comic['thumb'] }}" alt="img">
+            <p>{{ $comic['title'] }}</p>
+
+          </div>
+            
+        @endforeach
+    </div>
+  </div>
+</main>
+
 @endsection
+<!-- end content -->
