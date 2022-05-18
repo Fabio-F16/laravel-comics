8d3ecca33ee2b5363b3deda4ba086c86067a1078
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-18 21:48:34 +0200

    add cardDetails

---
+++ resources/views/cardDetails.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.app')
+
+@section('title')
+   laravel Card Details
+@endsection
+
+
+<!-- start content -->
+@section('content')
+
+  <div class="container">
+    siiii
+  </div>
+
+@endsection
+<!-- end content -->
