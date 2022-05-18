f7bf702d30985d83761bc7e3babd5153cfeee7ec
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-18 21:48:56 +0200

    add link cardDetails

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -12,10 +12,10 @@
     <div class="row justify-content-center">
       @foreach ($comics as $comic)
           <div class="col-12 col-md-2 m-3 text-center">
-            
-            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-            <p>{{ $comic['title'] }}</p>
-
+            <a href="cardDetails">
+              <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+              <p>{{ $comic['title'] }}</p>
+            </a>
           </div>
             
         @endforeach
