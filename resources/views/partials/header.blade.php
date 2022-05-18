133faf04e5621902487764f81b50f19d04829d2c
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-18 16:14:08 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,32 @@
-<header>
-        headerrrr
+<header class="container">
+    <div class="row justify-content-between align-items-center">
+
+        <div>
+            <img src="../img/dc-logo.png" alt="Logo" />
+        </div>
+
+        <div>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+
+        <div>
+            <input type="search" placeholder="search">
+        </div>
+
+    </div>
 </header>
+
+<div class="jumbotron">
+    <!-- <img src="../img/jumbotron.jpg" alt="Jumbotron" /> -->
+</div>
